ad96b2606f1828d30295438920f5bb3f32d48668
Aaron Stewart <astew@tristram.io>
2025-11-16 17:57:32 -0500

    Fixes importation issue

--- apps/ui/vite.config.js
+++ apps/ui/vite.config.js
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  optimizeDeps: {
+    include: ['@graph-battle/core'],
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    commonjsOptions: {
+      include: [/packages\/core\//, /node_modules/],
+    },
   },
 });
